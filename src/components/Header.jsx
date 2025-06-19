d98a9c54e610aa1bb7cc77f842c0e64a45fcd860
Swetha Kolan <swethakolan271@gmail.com>
2025-06-19 16:42:45 +0530

    Add complete React app excluding ignored files

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import HighlightIcon from "@mui/icons-material/Highlight";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <HighlightIcon />
+        NoteNest
+      </h1>
+      <p>A cozy place for all your notes</p>
+  
+    </header>
+  );
+}
+
+export default Header;
